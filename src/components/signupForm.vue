d39c8342df33a1b87c007f9d4cc8376fcda05f14
emmanuel lopez <emmanuel.lopez.g@icloud.com>
2021-06-25 13:34:58 -0500

    adding delete element

--- src/components/signupForm.vue
+++ src/components/signupForm.vue
@@ -12,9 +12,15 @@
           <option value="designer">Web Designer</option>
       </select>
 
+      <label>Skills:</label>  
+      <input type="text" v-model="tempSkill" @keyup.enter="addSkill">
+      <div v-for="skill in skills" :key="skill" class="pill" >
+        <span @click="deleteSkill(skill)">{{skill}}</span>
+      </div>
+
       <div class="terms">
           <input type="checkbox" required v-model="terms">
-          <label >Accept terms and conditions</label>
+          <label>Accept terms and conditions</label>
       </div>
 
   </form>
@@ -28,7 +34,24 @@ export default {
             email:'',
             password:'',
             role:'designer',
-            terms:'false'
+            terms:'false',
+            tempSkill:'',
+            skills:[]
+        }
+    },
+    methods:{
+        addSkill(e){
+            if(/*e.key ===',' &&*/ this.tempSkill){
+                if(!this.skills.includes(this.tempSkill)){
+                    this.skills.push(this.tempSkill)
+                }
+                this.tempSkill = ''
+            }
+        },
+        deleteSkill(skill){
+            this.skills = this.skills.filter((item) =>{
+                return item !== skill 
+            })
         }
     }
 }
@@ -76,5 +99,17 @@ input[type="checkbox"]{
     padding:15px 0;
 }
 
+.pill{
+    display: inline-block;
+    margin:20px 10px 0 0;
+    padding: 6px 12px;
+    background: #eee;
+    border-radius: 20px;
+    font-size:12px;
+    letter-spacing:1px;
+    font-weight: bold;
+    color: #777;
+    cursor: pointer;
+}
 
 </style>
