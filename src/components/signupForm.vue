71e916102eefafbd809c9ce60a57dd6796deeef1
emmanuel lopez <emmanuel.lopez.g@icloud.com>
2021-06-25 12:31:28 -0500

    adding inputs

--- src/components/signupForm.vue
+++ src/components/signupForm.vue
@@ -2,14 +2,35 @@
 <template>
   <form>
       <label>Email:</label>
-      <input type="email" required>
+      <input type="email" required v-model="email">
+      <label>Password:</label>
+      <input type="password" required v-model="password">
+      
+      <label>Role:</label>
+      <select v-model="role"> 
+          <option value="developer">Web Developer</option>
+          <option value="designer">Web Designer</option>
+      </select>
+
+      <div class="terms">
+          <input type="checkbox" required v-model="terms">
+          <label >Accept terms and conditions</label>
+      </div>
+
   </form>
 </template>
 
 <script>
 
 export default {
-
+    data(){
+        return{
+            email:'',
+            password:'',
+            role:'designer',
+            terms:'false'
+        }
+    }
 }
 </script>
 
@@ -33,7 +54,7 @@ label{
     font-weight: bold;
 }
 
-input{
+input, select{
     display: block;
     padding: 10px 6px;
     width: 100%;
@@ -41,6 +62,18 @@ input{
     border: none;
     border-bottom: 1px solid #ddd;
     color: #555;
+}
+
+input[type="checkbox"]{
+    display: inline-block;
+    width: 16px;
+    margin: 0 10px 0 0;
+    position:relative;
+    top: 2px;
+}
+
+.terms {
+    padding:15px 0;
 }
 
 
